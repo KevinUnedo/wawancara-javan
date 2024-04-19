fefa174fd3d50aca74814589c9e47799148d8c4b
Kevin Unedo Samosir <89372097+KevinUnedo@users.noreply.github.com>
2024-04-19 21:34:03 +0700

    Adding the task telambat

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [showProjectOptions, setShowProjectOptions] = useState(false);
   const [filteredAnalystOptions, setFilteredAnalystOptions] = useState([]);
   const [filteredProjectOptions, setFilteredProjectOptions] = useState([]);
-  const [additionalInputs, setAdditionalInputs] = useState([]);
+  const [additionalInputsTaskHariIni, setAdditionalInputsTaskHariIni] = useState([]);
+  const [additionalInputsTaskTerlambat, setAdditionalInputsTaskTerlambat] = useState([]);
 
   const analystOptions = ["Purwa Dazoratun", "Kevin Unedo"]; // List of analyst options
   const projectOptions = ["Icon+", "Iconhub", "Testing Project"]; // List of project options
@@ -48,20 +49,36 @@ function App() {
     setShowProjectOptions(true);
   };
 
-  const handleAddInputColumn = () => {
-    setAdditionalInputs([...additionalInputs, { namaAnggota: "", urlTask: "", statusTask: "" }]);
+  const handleAddInputColumn = (type) => {
+    if (type === 'taskHariIni') {
+      setAdditionalInputsTaskHariIni([...additionalInputsTaskHariIni, { namaAnggota: "", urlTask: "", statusTask: "" }]);
+    } else if (type === 'taskTerlambat') {
+      setAdditionalInputsTaskTerlambat([...additionalInputsTaskTerlambat, { namaAnggota: "", urlTask: "", statusTask: "" }]);
+    }
   };
 
-  const handleInputChange = (index, type, value) => {
-    const updatedInputs = [...additionalInputs];
-    updatedInputs[index][type] = value;
-    setAdditionalInputs(updatedInputs);
+  const handleInputChange = (index, type, value, inputType) => {
+    if (inputType === 'taskHariIni') {
+      const updatedInputs = [...additionalInputsTaskHariIni];
+      updatedInputs[index][type] = value;
+      setAdditionalInputsTaskHariIni(updatedInputs);
+    } else if (inputType === 'taskTerlambat') {
+      const updatedInputs = [...additionalInputsTaskTerlambat];
+      updatedInputs[index][type] = value;
+      setAdditionalInputsTaskTerlambat(updatedInputs);
+    }
   };
 
-  const handleRemoveInputColumn = (index) => {
-    const updatedInputs = [...additionalInputs];
-    updatedInputs.splice(index, 1);
-    setAdditionalInputs(updatedInputs);
+  const handleRemoveInputColumn = (index, inputType) => {
+    if (inputType === 'taskHariIni') {
+      const updatedInputs = [...additionalInputsTaskHariIni];
+      updatedInputs.splice(index, 1);
+      setAdditionalInputsTaskHariIni(updatedInputs);
+    } else if (inputType === 'taskTerlambat') {
+      const updatedInputs = [...additionalInputsTaskTerlambat];
+      updatedInputs.splice(index, 1);
+      setAdditionalInputsTaskTerlambat(updatedInputs);
+    }
   };
 
   return (
@@ -72,11 +89,51 @@ function App() {
         <div className="mt-2 flex">
           <div className="flex-1 pr-2">
             <h3 className='mt-3'>Analyst Name</h3>
-            {/* Analyst Name Input */}
+            <div className="mt-2 relative">
+              <input
+                type="text"
+                value={analystName}
+                onChange={handleAnalystInputChange}
+                onClick={handleAnalystInputClick}
+                className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 cursor-pointer"
+                placeholder="Select..."
+              />
+              <div className={`absolute z-10 top-full left-0 w-full bg-white border border-gray-300 shadow-lg rounded-md mt-1 ${showAnalystOptions ? '' : 'hidden'}`}>
+                {filteredAnalystOptions.map((option, index) => (
+                  <div
+                    key={index}
+                    className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => handleAnalystOptionClick(option)}
+                  >
+                    {option}
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
           <div className="flex-1 pl-2 mt-3">
             <h5 className=''>Project</h5>
-            {/* Project Input */}
+            <div className="mt-2 relative">
+              <input
+                type="text"
+                value={project}
+                onChange={handleProjectInputChange}
+                onClick={handleProjectInputClick}
+                className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 cursor-pointer"
+                placeholder="Select..."
+              />
+              <div className={`absolute z-10 top-full left-0 w-full bg-white border border-gray-300 shadow-lg rounded-md mt-1 ${showProjectOptions ? '' : 'hidden'}`}>
+                {filteredProjectOptions.map((option, index) => (
+                  <div
+                    key={index}
+                    className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => handleProjectOptionClick(option)}
+                  >
+                    {option}
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
         <div className='mt-4 py-4 border border-gray-300 rounded-md'>
@@ -93,17 +150,20 @@ function App() {
               <p className="p-2">Status Task</p>
             </div>
             <div className="col-span-1 flex items-center justify-center">
-              <button onClick={handleAddInputColumn} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <button onClick={() => handleAddInputColumn('taskHariIni')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 +
               </button>
             </div>
-            {additionalInputs.map((input, index) => (
-              <React.Fragment key={`input-${index}`}>
+          </div>
+          <hr className="border-gray-200 w-full m-0" />
+          <div className='grid grid-cols-4 gap-4 p-4'>
+          {additionalInputsTaskHariIni.map((input, index) => (
+              <React.Fragment key={`input-task-hari-ini-${index}`}>
                 <div className="col-span-1">
                   <input
                     type="text"
                     value={input.namaAnggota}
-                    onChange={(e) => handleInputChange(index, 'namaAnggota', e.target.value)}
+                    onChange={(e) => handleInputChange(index, 'namaAnggota', e.target.value, 'taskHariIni')}
                     className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                   />
                 </div>
@@ -111,7 +171,7 @@ function App() {
                   <input
                     type="text"
                     value={input.urlTask}
-                    onChange={(e) => handleInputChange(index, 'urlTask', e.target.value)}
+                    onChange={(e) => handleInputChange(index, 'urlTask', e.target.value, 'taskHariIni')}
                     className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                   />
                 </div>
@@ -119,19 +179,74 @@ function App() {
                   <input
                     type="text"
                     value={input.statusTask}
-                    onChange={(e) => handleInputChange(index, 'statusTask', e.target.value)}
+                    onChange={(e) => handleInputChange(index, 'statusTask', e.target.value, 'taskHariIni')}
                     className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                   />
                 </div>
                 <div className="col-span-1 flex items-center justify-center">
-                  <button onClick={() => handleRemoveInputColumn(index)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                  <button onClick={() => handleRemoveInputColumn(index, 'taskHariIni')} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                     -
                   </button>
                 </div>
               </React.Fragment>
             ))}
           </div>
+        </div>
+        <div className='mt-4 py-4 border border-gray-300 rounded-md'>
+          <h5 className='px-4 mb-4'>Task Terlambat</h5>
           <hr className="border-gray-200 w-full m-0" />
+          <div className='bg-slate-100 grid grid-cols-4 gap-4 p-4'>
+            <div className="col-span-1">
+              <p className="p-2">Nama Anggota</p>
+            </div>
+            <div className="col-span-1">
+              <p className="p-2">Url Task</p>
+            </div>
+            <div className="col-span-1 ">
+              <p className="p-2">Status Task</p>
+            </div>
+            <div className="col-span-1 flex items-center justify-center">
+              <button onClick={() => handleAddInputColumn('taskTerlambat')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                +
+              </button>
+            </div>
+          </div>
+          <hr className="border-gray-200 w-full m-0" />
+          <div className='grid grid-cols-4 gap-4 p-4'>
+          {additionalInputsTaskTerlambat.map((input, index) => (
+              <React.Fragment key={`input-task-terlambat-${index}`}>
+                <div className="col-span-1">
+                  <input
+                    type="text"
+                    value={input.namaAnggota}
+                    onChange={(e) => handleInputChange(index, 'namaAnggota', e.target.value, 'taskTerlambat')}
+                    className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <input
+                    type="text"
+                    value={input.urlTask}
+                    onChange={(e) => handleInputChange(index, 'urlTask', e.target.value, 'taskTerlambat')}
+                    className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <input
+                    type="text"
+                    value={input.statusTask}
+                    onChange={(e) => handleInputChange(index, 'statusTask', e.target.value, 'taskTerlambat')}
+                    className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  />
+                </div>
+                <div className="col-span-1 flex items-center justify-center">
+                  <button onClick={() => handleRemoveInputColumn(index, 'taskTerlambat')} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    -
+                  </button>
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
         </div>
       </div>
     </div>
