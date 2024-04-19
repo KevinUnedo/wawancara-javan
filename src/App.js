cf5b84f00adcf8518c4d1c80495b0caee8e8d193
Kevin Unedo Samosir <89372097+KevinUnedo@users.noreply.github.com>
2024-04-19 18:41:10 +0700

    Add the input field

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [showProjectOptions, setShowProjectOptions] = useState(false);
   const [filteredAnalystOptions, setFilteredAnalystOptions] = useState([]);
   const [filteredProjectOptions, setFilteredProjectOptions] = useState([]);
-  
+  const [additionalInputs, setAdditionalInputs] = useState([]);
+
   const analystOptions = ["Purwa Dazoratun", "Kevin Unedo"]; // List of analyst options
   const projectOptions = ["Icon+", "Iconhub", "Testing Project"]; // List of project options
 
@@ -47,60 +48,36 @@ function App() {
     setShowProjectOptions(true);
   };
 
+  const handleAddInputColumn = () => {
+    setAdditionalInputs([...additionalInputs, { namaAnggota: "", urlTask: "", statusTask: "" }]);
+  };
+
+  const handleInputChange = (index, type, value) => {
+    const updatedInputs = [...additionalInputs];
+    updatedInputs[index][type] = value;
+    setAdditionalInputs(updatedInputs);
+  };
+
+  const handleRemoveInputColumn = (index) => {
+    const updatedInputs = [...additionalInputs];
+    updatedInputs.splice(index, 1);
+    setAdditionalInputs(updatedInputs);
+  };
+
   return (
     <div className="container mx-auto mt-4 px-4 pt-3 pb-4 rounded-lg border border-gray-200">
       <strong className="text-lg px-4">Daily Scrum</strong>
       <hr className="my-2 border-gray-200" />
       <div className='px-4'>
-      <div className="mt-2 flex">
-        <div className="flex-1 pr-2">
-          <h3 className='mt-3'>Analyst Name</h3>
-          <div className="mt-2 relative">
-            <input
-              type="text"
-              value={analystName}
-              onChange={handleAnalystInputChange}
-              onClick={handleAnalystInputClick}
-              className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 cursor-pointer"
-              placeholder="Select..."
-            />
-            <div className={`absolute z-10 top-full left-0 w-full bg-white border border-gray-300 shadow-lg rounded-md mt-1 ${showAnalystOptions ? '' : 'hidden'}`}>
-              {filteredAnalystOptions.map((option, index) => (
-                <div
-                  key={index}
-                  className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
-                  onClick={() => handleAnalystOptionClick(option)}
-                >
-                  {option}
-                </div>
-              ))}
-            </div>
+        <div className="mt-2 flex">
+          <div className="flex-1 pr-2">
+            <h3 className='mt-3'>Analyst Name</h3>
+            {/* Analyst Name Input */}
           </div>
-        </div>
-        <div className="flex-1 pl-2 mt-3">
-          <h5 className=''>Project</h5>
-          <div className="mt-2 relative">
-            <input
-              type="text"
-              value={project}
-              onChange={handleProjectInputChange}
-              onClick={handleProjectInputClick}
-              className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 cursor-pointer"
-              placeholder="Select..."
-            />
-            <div className={`absolute z-10 top-full left-0 w-full bg-white border border-gray-300 shadow-lg rounded-md mt-1 ${showProjectOptions ? '' : 'hidden'}`}>
-              {filteredProjectOptions.map((option, index) => (
-                <div
-                  key={index}
-                  className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
-                  onClick={() => handleProjectOptionClick(option)}
-                >
-                  {option}
-                </div>
-              ))}
-            </div>
+          <div className="flex-1 pl-2 mt-3">
+            <h5 className=''>Project</h5>
+            {/* Project Input */}
           </div>
-        </div>
         </div>
         <div className='mt-4 py-4 border border-gray-300 rounded-md'>
           <h5 className='px-4 mb-4'>Target Hari Ini</h5>
@@ -112,13 +89,49 @@ function App() {
             <div className="col-span-1">
               <p className="p-2">Url Task</p>
             </div>
-            <div className="col-span-1">
+            <div className="col-span-1 ">
               <p className="p-2">Status Task</p>
             </div>
             <div className="col-span-1 flex items-center justify-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+</button>
+              <button onClick={handleAddInputColumn} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                +
+              </button>
             </div>
+            {additionalInputs.map((input, index) => (
+              <React.Fragment key={`input-${index}`}>
+                <div className="col-span-1">
+                  <input
+                    type="text"
+                    value={input.namaAnggota}
+                    onChange={(e) => handleInputChange(index, 'namaAnggota', e.target.value)}
+                    className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <input
+                    type="text"
+                    value={input.urlTask}
+                    onChange={(e) => handleInputChange(index, 'urlTask', e.target.value)}
+                    className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <input
+                    type="text"
+                    value={input.statusTask}
+                    onChange={(e) => handleInputChange(index, 'statusTask', e.target.value)}
+                    className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  />
+                </div>
+                <div className="col-span-1 flex items-center justify-center">
+                  <button onClick={() => handleRemoveInputColumn(index)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    -
+                  </button>
+                </div>
+              </React.Fragment>
+            ))}
           </div>
+          <hr className="border-gray-200 w-full m-0" />
         </div>
       </div>
     </div>
